48bfa87c4a19f8930e5ba20a8028b0cc018c4e9c
Sébastien Castiel <sebastien@castiel.me>
2018-09-24 16:57:24 -0400

    Improve comment form to add date at submit

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
 import styled from 'styled-components'
 
 const StyledInput = styled.input`
@@ -12,6 +12,7 @@ const StyledTextArea = styled.textarea`
   padding: 0.3rem;
   font-size: 1rem;
   resize: vertical;
+  height: 5em;
 `
 const StyledLabel = styled.label`
   display: flex;
@@ -25,45 +26,57 @@ const StyledButton = styled.button`
   padding: 0.3em 1em;
 `
 
-const CommentForm = ({ slug }) => {
-  return (
-    <form
-      name="comment"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="slug" value={slug} />
-      <input type="hidden" name="form-name" value="comment" />
+class CommentForm extends Component {
+  constructor(props) {
+    super(props)
+    this.dateInput = createRef()
+  }
+  onSubmit = event => {
+    this.dateInput.current.value = new Date().toISOString()
+  }
+  render() {
+    const { slug } = this.props
+    return (
+      <form
+        name="comment"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.onSubmit}
+      >
+        <input type="hidden" name="slug" value={slug} />
+        <input type="hidden" name="form-name" value="comment" />
+        <input name="date" type="hidden" ref={this.dateInput} />
 
-      <hr />
-      <h4>Leave a comment</h4>
-      <p hidden>
-        <label>
-          Don’t fill this out: <input name="bot-field" />
-        </label>
-      </p>
-      <p>
-        <StyledLabel>
-          Your Name: <StyledInput required type="text" name="name" />
-        </StyledLabel>
-      </p>
-      <p>
-        <StyledLabel>
-          Your Email: <StyledInput required type="email" name="email" />
-        </StyledLabel>
-      </p>
-      <p>
-        <StyledLabel>
-          Comment: <StyledTextArea required name="message" />
-        </StyledLabel>
-      </p>
-      <div data-netlify-recaptcha="true" />
-      <p>
-        <StyledButton type="submit">Send</StyledButton>
-      </p>
-    </form>
-  )
+        <hr />
+        <h4>Leave a comment</h4>
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
+        <p>
+          <StyledLabel>
+            Your Name: <StyledInput required type="text" name="name" />
+          </StyledLabel>
+        </p>
+        <p>
+          <StyledLabel>
+            Your Email: <StyledInput required type="email" name="email" />
+          </StyledLabel>
+        </p>
+        <p>
+          <StyledLabel>
+            Comment: <StyledTextArea required name="message" />
+          </StyledLabel>
+        </p>
+        <div data-netlify-recaptcha="true" />
+        <p>
+          <StyledButton type="submit">Send</StyledButton>
+        </p>
+      </form>
+    )
+  }
 }
 
 export default CommentForm
