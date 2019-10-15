081766aa7bf2617ef41b96a6a4782c02ff179299
Sébastien Castiel <sebastien@castiel.me>
2019-10-15 18:26:57 -0400

    Revert "Fix theme for server side rendering"
    This reverts commit 44f23e19967fd613c1756c493cc80ef73a602b79.

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -1,10 +1,1 @@
 export const onServiceWorkerUpdateReady = () => window.location.reload(true)
-
-export const onClientEntry = () => {
-  window.__theme =
-    (window.localStorage && window.localStorage.getItem('theme')) ||
-    (window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light')
-}
